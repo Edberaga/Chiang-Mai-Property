9f42164aa3fa7206cdba500aa5d77482032cacb6
Edberaga <edbetjonnathan@gmail.com>
2023-10-18 23:43:55 +0700

    Add the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+
+//Page Imports
 import Home from './pages/Home'
 import Profile from './pages/Account/Profile'
 import Signin from './pages/Account/Signin'
 import Signup from './pages/Account/Signup'
 import ForgotPass from './pages/Account/ForgotPass'
+import Header from './components/Header'
+import Offers from './pages/Offers'
 
 function App() {
-
+  console.log("It is rendering!");
   return (
   <>
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/offers" element={<Offers/>} />
 
         {/*Account routes */}
         <Route path="/profile" element={<Profile/>}/>
