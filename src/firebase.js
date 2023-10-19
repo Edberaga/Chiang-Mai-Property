a3a6921614b82a9af4d27512e567ecffaa8a2f41
Edberaga <edbetjonnathan@gmail.com>
2023-10-19 23:06:50 +0700

    Fix the environment declaration

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
 const firebaseConfig = {
-  apiKey: import.meta.key.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.key.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.key.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.key.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.key.VITE_FIREBASE_SENDER_ID ,
-  appId: import.meta.key.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.key.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID ,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
