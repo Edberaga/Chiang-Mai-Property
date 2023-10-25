add6d7cb07454c8ea9232d85087d708adaea0069
Edberaga <edbetjonnathan@gmail.com>
2023-10-25 23:04:27 +0700

    Add getStorage features

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 console.log(import.meta.env.VITE_FIREBASE_API_KEY);
 const firebaseConfig = {
@@ -21,3 +22,4 @@ const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
