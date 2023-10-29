93f4ae6c2721f3196705fc2458385f6dfe8c2175
Edberaga <edbetjonnathan@gmail.com>
2023-10-29 17:05:48 +0700

    Only need to hide the API KEY

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,7 @@ import mkcert from'vite-plugin-mkcert'
 export default defineConfig({
   plugins: [react(), mkcert()],
   define:{
-    'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
-    'process.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
-    'process.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID), 
-    'process.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.VITE_FIREBASE_STORAGE_BUCKET),
-    'process.env.VITE_FIREBASE_SENDER_ID': JSON.stringify(process.env.VITE_FIREBASE_SENDER_ID),
-    'process.env.VITE_FIREBASE_APP_ID': JSON.stringify(process.env.VITE_FIREBASE_APP_ID),
-    'process.env.VITE_FIREBASE_MEASUREMENT_ID ': JSON.stringify(process.env.VITE_FIREBASE_MEASUREMENT_ID ),
+    'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY)
   },
   server: {
     https: true
